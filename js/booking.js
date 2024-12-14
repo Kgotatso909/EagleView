925c6167b094a455607747f4ddecb439ec97ae9a
Kgotatso Nelson Mohlala <121243406+Kgotatso909@users.noreply.github.com>
2024-12-14 14:51:46 +0200

    a

--- js/booking.js
+++ js/booking.js
@@ -40,16 +40,15 @@ function validateDates() {
   const checkin = document.getElementById("checkin").value;
   const checkout = document.getElementById("checkout").value;
 
-  // Check if both checkin and checkout dates are selected
+  
   if (!checkin || !checkout) {
     return false;
   }
 
-  // Convert to Date objects for comparison
+  
   const checkinDate = new Date(checkin);
   const checkoutDate = new Date(checkout);
 
-  // Check if check-out is before check-in
   if (checkoutDate <= checkinDate) {
     return false;
   }
@@ -61,36 +60,34 @@ function validateDates() {
 bookingForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  // Basic Validation
+ 
   if (!bookingForm.checkValidity() || !validateDates()) {
     bookingForm.classList.add("was-validated");
 
-    // Display custom error for date fields
+    s
     if (!validateDates()) {
+      const checkinInput = document.getElementById("checkin");
       const checkoutInput = document.getElementById("checkout");
       const checkoutError = document.getElementById("checkoutError");
+      const checkinError = document.getElementById("checkoutError");
 
-      // Add the invalid class to checkout field
+      checkinInput.classList.add("is-invalid")
       checkoutInput.classList.add("is-invalid");
 
-      // Display error message
+      checkinError.style.display = "block";
       checkoutError.style.display = "block";
     }
 
     return;
   }
 
-  // Show Toast
   showToast();
 
-  // Reset Form
   bookingForm.reset();
   bookingForm.classList.remove("was-validated");
 
-  // Hide error message after successful submission
-  const checkoutInput = document.getElementById("checkout");
-  const checkoutError = document.getElementById("checkoutError");
-
+  checkinInput.classList.remove("is-invalid");
   checkoutInput.classList.remove("is-invalid");
+  checkinError.style.display = "none"
   checkoutError.style.display = "none";
 });
